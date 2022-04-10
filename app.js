ff3e20a59dec7d61adc6cc8af9ee7c48c85932be
hussainHakimi <hussainbayan614@gmail.com>
2022-04-10 16:29:34 +0430

    all the function changed to arrow function.

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
 
+const RESULT_DRAW = 'DRAW';
+const RESULT_COMPUTER_WIN = 'COMOUTER WIN';
+const RESULT_PLAYER_WIN = 'PLAYER WIN';
+
 let gameIsRunnig = false;
-const getUserChoice = function(){
+const getUserChoice = () => {  // using => arrow function.
     const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS} ?`, '').toUpperCase();
     if(selection !== ROCK && selection !== PAPER && selection !== SCISSORS){
         alert(`Invalid Choice!, we choose ${DEFAULT_USER_CHOICE} for defualt`);
@@ -24,7 +28,42 @@ const getUserChoice = function(){
     }
         return selection;
 };
-startGameBtn.addEventListener('click', function(){ // this function is known as anonymous function.
+
+const getComputerChoice = () => {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.34){
+        return ROCK;
+    }else if(randomNumber < 0.67 && randomNumber > 0.34){
+        return PAPER;
+    }else if(randomNumber > 0.67 && randomNumber < 1){
+        return SCISSORS;
+    }
+}
+
+const getWiner = (cChoice, pChoice) =>{
+    return cChoice === pChoice ? RESULT_DRAW
+        :   cChoice === ROCK && pChoice === PAPER ||
+            cChoice === PAPER  && pChoice === SCISSORS ||
+            cChoice === SCISSORS && pChoice === ROCK
+        ? RESULT_PLAYER_WIN 
+        : RESULT_COMPUTER_WIN;
+
+
+
+    // if(cChoice === pChoice){
+    //     return RESULT_DRAW;
+    // }else if (
+    //     cChoice === ROCK && pChoice === PAPER ||
+    //     cChoice === PAPER  && pChoice === SCISSORS ||
+    //     cChoice === SCISSORS && pChoice === ROCK
+    //     ){
+    //     return RESULT_PLAYER_WIN;
+    // }else{
+    //     return RESULT_COMPUTER_WIN;
+    // }
+}
+
+startGameBtn.addEventListener('click', () => { // this function is known as anonymous function.
     if(gameIsRunnig){
         return;
     }
@@ -32,5 +71,9 @@ startGameBtn.addEventListener('click', function(){ // this function is known as 
     console.log('Game is starting...');
 
     const playerChoice = getUserChoice();
+    const computerChoice = getComputerChoice();
+    const winner = getWiner(computerChoice, playerChoice);
     console.log(playerChoice);
+    console.log(computerChoice);
+    console.log(winner);
 });
