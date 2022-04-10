4d0ef656085694f7c52ed880c6fe2cb0df6beb71
hussainHakimi <hussainbayan614@gmail.com>
2022-04-10 14:56:44 +0430

    the codes for starting game is done.

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame(){
-	
-}
+// const start = function(){
+// 	console.log('Game is starting...');
+// }
+// const person = {
+//     greet: function greet(){
+//         console.log('Hello there.')
+//     }
+// }
+// person.greet();
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunnig = false;
+const getUserChoice = function(){
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS} ?`, '').toUpperCase();
+    if(selection !== ROCK && selection !== PAPER && selection !== SCISSORS){
+        alert(`Invalid Choice!, we choose ${DEFAULT_USER_CHOICE} for defualt`);
+        return DEFAULT_USER_CHOICE;
+    }
+        return selection;
+};
+startGameBtn.addEventListener('click', function(){ // this function is known as anonymous function.
+    if(gameIsRunnig){
+        return;
+    }
+    gameIsRunnig = true;
+    console.log('Game is starting...');
+
+    const playerChoice = getUserChoice();
+    console.log(playerChoice);
+});
